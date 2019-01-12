34d365014f34b921c42310bf031f215945a5e92d
Maid Dog <maiddogsrl@gmail.com>
2019-01-12 02:57:27 -0600

    Complete day 3

--- lib/base/src/lib.rs
+++ lib/base/src/lib.rs
@@ -9,8 +9,8 @@ pub fn run(day: u32) -> Result<(), std::io::Error> {
         1 => println!("{}, {}", day1::part_1()?, day1::part_2()?),
         2 => println!("{}, {}", day2::part_1()?, day2::part_2()?),
         3 => println!("{}, {}", day3::part_1()?, day3::part_2()?),
-        4 => println!("{}, {}", day4::part_1()?, day4::part_2()?),
-        5 => println!("{}, {}", day5::part_1()?, day5::part_2()?),
+        // 4 => println!("{}, {}", day4::part_1()?, day4::part_2()?),
+        // 5 => println!("{}, {}", day5::part_1()?, day5::part_2()?),
         _ => println!("Invalid day."),
     }
 
--- lib/day3/src/lib.rs
+++ lib/day3/src/lib.rs
@@ -1,15 +1,16 @@
 #[derive(Debug)]
 struct Rect {
-    x: i32,
-    y: i32,
-    w: i32,
-    h: i32,
+    pub id: i32,
+    pub x: i32,
+    pub y: i32,
+    pub w: i32,
+    pub h: i32,
 }
 
 impl Rect {
-    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+    pub fn new(id: i32, x: i32, y: i32, w: i32, h: i32) -> Self {
         Self {
-            x, y, w, h,
+            id, x, y, w, h,
         }
     }
     
@@ -29,7 +30,7 @@ impl Rect {
         let w = std::cmp::min(self.x + self.w, other.x + other.w) - x;
         let h = std::cmp::min(self.y + other.h, other.y + other.h) - y;
 
-        Some(Rect::new(x, y, w, h))
+        Some(Rect::new(0, x, y, w, h))
     }
 }
 
@@ -43,6 +44,7 @@ fn read_rects(content: &str) -> Vec<Rect> {
             .collect::<Vec<i32>>();
 
         rects.push(Rect::new(
+            split[0],
             split[1],
             split[2],
             split[3],
@@ -57,23 +59,41 @@ pub fn part_1() -> Result<i32, std::io::Error> {
     let content = std::fs::read_to_string("input/3.txt")?;
 
     let rects = read_rects(&content);
-    let mut overlaps = Vec::new();
+    let mut area = std::collections::HashSet::new();
+    let mut overlap = std::collections::HashSet::new();
 
-    for (current, rect) in rects.iter().enumerate() {
-        for index in current + 1..rects.len() {
-            if let Some(overlap) = rect.overlap(&rects[index]) {
-                println!("Overlap: {:?} {:?} => {:?}", rect, rects[index], overlap);
-                overlaps.push(overlap);
+    for rect in &rects {
+        for x in rect.x..rect.x + rect.w {
+            for y in rect.y..rect.y + rect.h {
+                if !area.insert((x, y)) {
+                    overlap.insert((x, y));
+                }
             }
         }
     }
 
-    let total_area: i32 = rects.iter().map(|r| r.area()).sum();
-    let overlap_area: i32 = overlaps.iter().map(|r| r.area()).sum();
-
-    Ok(total_area - overlap_area)
+    Ok(overlap.iter().count() as i32)
 }
 
 pub fn part_2() -> Result<i32, std::io::Error> {
+    let content = std::fs::read_to_string("input/3.txt")?;
+
+    let rects = read_rects(&content);
+
+    for (current, rect) in rects.iter().enumerate() {
+        let mut overlapped = false;
+
+        for index in 0..rects.len() {
+            if current != index && rect.overlap(&rects[index]).is_some() {
+                overlapped = true;
+                break;
+            }
+        }
+
+        if !overlapped {
+            return Ok(rect.id);
+        }
+    }
+
     Ok(0)
 }
