8eaba353e6e3bbce651f0b539add8e2df3de4369
Maid Dog <maiddogsrl@gmail.com>
2019-01-12 05:53:41 -0600

    Add day 6

--- lib/base/src/lib.rs
+++ lib/base/src/lib.rs
@@ -6,11 +6,12 @@ pub fn run(day: u32) -> Result<(), std::io::Error> {
     print!("Day {}: ", day);
 
     match day {
-        1 => println!("{}, {}", day1::part_1()?, day1::part_2()?),
-        2 => println!("{}, {}", day2::part_1()?, day2::part_2()?),
-        3 => println!("{}, {}", day3::part_1()?, day3::part_2()?),
-        4 => println!("{}, {}", day4::part_1()?, day4::part_2()?),
-        5 => println!("{}, {}", day5::part_1()?, day5::part_2()?),
+        1 => println!("{} | {}", day1::part_1()?, day1::part_2()?),
+        2 => println!("{} | {}", day2::part_1()?, day2::part_2()?),
+        3 => println!("{} | {}", day3::part_1()?, day3::part_2()?),
+        4 => println!("{} | {}", day4::part_1()?, day4::part_2()?),
+        5 => println!("{} | {}", day5::part_1()?, day5::part_2()?),
+        6 => println!("{} | {}", day6::part_1()?, day6::part_2()?),
         _ => println!("Invalid day."),
     }
 
---
+++ lib/day6/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn part_1() -> Result<i32, std::io::Error> {
+    Ok(0)
+}
+
+pub fn part_2() -> Result<i32, std::io::Error> {
+    Ok(0)
+}
